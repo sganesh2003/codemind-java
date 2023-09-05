f94ef781552b8066e79c6d0b560e6d1e1421ce3a
sganesh2003 <121417459+sganesh2003@users.noreply.github.com>
2023-09-05 11:25:45 +0530

    import java.util.*; public class Ganesh{     public static void main(String[]Args){         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         double a=((3.14)*r*r);         double b=((3.14)*2*r);         System.out.printf("%.2f",a);         System.out.println();         System.out.printf("%.2f",b);     } }

---
+++ Area_and_Perimeter_of_Circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Ganesh{
+    public static void main(String[]Args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double a=((3.14)*r*r);
+        double b=((3.14)*2*r);
+        System.out.printf("%.2f",a);
+        System.out.println();
+        System.out.printf("%.2f",b);
+    }
+}
