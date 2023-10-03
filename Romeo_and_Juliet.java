adf9558f187e28851015ae9addcfeedff6948276
sganesh2003 <121417459+sganesh2003@users.noreply.github.com>
2023-10-03 15:25:47 +0530

    import java.util.*; public class Test{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int z=sc.nextInt();         int c=(n*5+m*10)/z;         System.out.println(c);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Test{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int z=sc.nextInt();
+        int c=(n*5+m*10)/z;
+        System.out.println(c);
+    }
+}
